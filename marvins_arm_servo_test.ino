f37d155dad770e9f645835a5bb7771555dc9c5a5
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-07-06 15:54:30 -0700

    Update marvins_arm_servo_test.ino

--- marvins_arm_servo_test.ino
+++ marvins_arm_servo_test.ino
@@ -17,8 +17,22 @@
 Servo servo_test;
 
 void setup() {
-  servo_test.attach(5);  // control pin 9
-  servo_test.writeMicroseconds(1500);  // center position
+  // servo_test.attach(5);  // Twist
+  // servo_test.writeMicroseconds(1484);  // Home position 1484 Twist
+
+  // servo_test.attach(6);  // Shoulder
+  // servo_test.writeMicroseconds(2000);  // Home position 2000 Shoulder
+
+  // servo_test.attach(9);  // Elbow
+  // servo_test.writeMicroseconds(1000);  // Home position 1000;  // closed (folded) Elbow
+
+  // servo_test.attach(10);  // Wrist
+  // servo_test.writeMicroseconds(1444);  // Home position 1444;  // straight out Wrist //posWrist = 1575;  // straight out
+
+  // servo_test.attach(11);  // Gripper
+  // servo_test.writeMicroseconds(1200);  // Home position 1200 Gripper (open?)
+
+  
 }
 
 void loop() {
